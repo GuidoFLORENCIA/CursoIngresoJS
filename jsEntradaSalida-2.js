92760282abfac9657911aa664939740a2cae45c1
GuidoFLORENCIA <malflor_44@hotmail.com>
2017-02-01 12:01:33 -0300

    Add files via upload

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,23 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar() 
+
+{  // esto es un comentario de una linea 
+	/* la la la 
+	la la la */ 
+	var nombre;  // defino variable 
+	nombre =" florencia";  
+
+	nombre = prompt("ingrese nu nombre","natali natalia");
+
+
+
+
+
+
+	alert  ("nombre  "+ nombre); 
+
+	 var person = prompt("ingresar tu nombre", "natalia natalia"); 
+	
+} 
+
